160306e66783813085f79a9ae393817a02568da0
Sarah Arshad <sarshad1@yahoo.co.uk>
2022-06-07 12:06:12 +0100

    added associations to models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :plants, dependent: :destroy
+  has_many :bookings, dependent: :destroy
+  has_one_attached :photo
 end
