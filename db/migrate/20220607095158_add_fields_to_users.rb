a73d254ee48c00f76ba7056d87ac4afcb3a2d429
Stephanie Wood <stephwood10@hotmail.com>
2022-06-07 11:08:53 +0100

    Created users model

---
+++ db/migrate/20220607095158_add_fields_to_users.rb
@@ -0,0 +1,9 @@
+class AddFieldsToUsers < ActiveRecord::Migration[7.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :string
+    add_column :users, :postcode, :string
+    add_column :users, :average_rating, :float
+  end
+end
