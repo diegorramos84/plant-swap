c1b793a4c6ccacc315e97dad207b45bd38ebbdf3
Sarah Arshad <sarshad1@yahoo.co.uk>
2022-06-08 11:11:16 +0100

    added a reviews controller and methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,5 @@ class User < ApplicationRecord
   has_many :plants, dependent: :destroy
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
+  has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
-  resources :users, only: %i[index show]
+  resources :users, only: %i[index show] do
+    resources :reviews, only: %i[new create show]
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
