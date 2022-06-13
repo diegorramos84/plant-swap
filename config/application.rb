ec06d1fc085609aa7f9bb9ea41312070b7844b89
Diego Ramos <diegorramos84@gmail.com>
2022-06-13 17:45:38 +0100

    fix heroku issue with static assets

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,7 @@ module PlantSwap
     end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.serve_static_assets = true
 
     # Configuration for the application, engines, and railties goes here.
     #
