865e7692dfb4e7f1ccf8dd9adf742c59c434c1e4
Diego Ramos <diegorramos84@gmail.com>
2022-06-14 16:25:36 +0100

    added 404/422/500 custom error pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'errors/not_found'
+  get 'errors/internal_server_error'
   get 'rooms/index'
   devise_for :users
   root to: "pages#home"
@@ -21,6 +23,7 @@ Rails.application.routes.draw do
     resources :messages, only: :create
   end
 
-
-
+  match "/404", to: "errors#not_found", via: :all
+  match "/500", to: "errors#internal_server_error", via: :all
+  match "/422", to: "errors#unprocessable_entity", via: :all
 end
