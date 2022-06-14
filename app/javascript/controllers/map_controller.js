c458253ce09cc3f7f9839cfddfb1b9081e8b1452
Sarah Arshad <sarshad1@yahoo.co.uk>
2022-06-14 11:45:01 +0100

    made map icons larger

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -29,8 +29,8 @@ export default class extends Controller {
       customMarker.className = "marker"
       customMarker.style.backgroundImage = `url('${marker.image_url}')`
       customMarker.style.backgroundSize = "contain"
-      customMarker.style.width = "30px"
-      customMarker.style.height = "30px"
+      customMarker.style.width = "35px"
+      customMarker.style.height = "35px"
 
       // Pass the element as an argument to the new marker
       new mapboxgl.Marker(customMarker)
