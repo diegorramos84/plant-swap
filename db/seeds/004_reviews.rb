163cd7fc9b3b14f798134061d72f5fd8e81b64b3
Sarah Arshad <sarshad1@yahoo.co.uk>
2022-06-14 15:34:44 +0100

    removed typos, updated seeds to match dropdown, updated booking quantity so you can't book 0 plants in dropdown

--- db/seeds/004_reviews.rb
+++ db/seeds/004_reviews.rb
@@ -1,29 +1,29 @@
 @review1 = Review.create!(
-  content: "Gorgeous plant! User was in the agreeded place on time for the plant-swap",
+  content: "Gorgeous plant! User was in the agreed place on time to plant-swap",
   rating: 5,
-  booking_id: @booking1[:id],
+  booking_id: @booking1[:id]
 )
 
 @review2 = Review.create!(
-  content: "Gorgeous plant! User was in the agreeded place on time for the plant-swap",
+  content: "Gorgeous plant! User was in the agreed place on time to plant-swap",
   rating: 5,
-  booking_id: @booking2[:id],
+  booking_id: @booking2[:id]
 )
 
 @review3 = Review.create!(
-  content: "Gorgeous plant! User was in the agreeded place on time for the plant-swap",
+  content: "Gorgeous plant! User was in the agreed place on time to plant-swap",
   rating: 5,
-  booking_id: @booking3[:id],
+  booking_id: @booking3[:id]
 )
 
 @review4 = Review.create!(
-  content: "Gorgeous plant! User was in the agreeded place on time for the plant-swap",
+  content: "Gorgeous plant! User was in the agreed place on time to plant-swap",
   rating: 5,
-  booking_id: @booking4[:id],
+  booking_id: @booking4[:id]
 )
 
 @review5 = Review.create!(
-  content: "Gorgeous plant! User was in the agreeded place on time for the plant-swap",
+  content: "Gorgeous plant! User was in the agreed place on time to plant-swap",
   rating: 5,
-  booking_id: @booking5[:id],
+  booking_id: @booking5[:id]
 )
