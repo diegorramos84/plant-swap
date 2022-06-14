a64065a58f133df9927591b21c75d7acd641b903
Sarah Arshad <sarshad1@yahoo.co.uk>
2022-06-14 14:13:28 +0100

    changed new review button to pink one

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -1,6 +1,6 @@
 class PlantsController < ApplicationController
   before_action :set_plant, only: [:show, :edit, :update, :destroy]
-  skip_before_action :authenticate_user!, only: [:index, :show]
+  skip_before_action :authenticate_user!, only: :index
 
   def index
     if params[:query].present?
